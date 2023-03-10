23f46e53e87b0bff4f1551527a961307c0b56d74
alexndra1272 <alexandrag1272@gmail.com>
2023-03-09 22:52:18 -0600

    Nav responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".lista");
+
+/*Adyudaaaa*/
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("visible-m");
+});
